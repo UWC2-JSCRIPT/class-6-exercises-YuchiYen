b39ac54c02e508c98be4d586a0ec2d10d96ca272
YuchiYen <yuchiyen@hotmail.com>
2022-11-23 21:47:35 -0800

    completed

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,35 @@
+$(document).ready(function() {
+
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
-
+    const $a = $('<a>');
+    $a.text('Buy Now!');
+    $a.attr('id','cta')
+    
+    // let lastP = $('p').last();
+    // lastP.append($a);
+    $('main').append($a);
+  
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+//var color = $('img').attr('data-color');
+console.log(`img data-color is: ${$('img').attr('data-color')} `);
+
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
+//$('li:eq(2)').attr('class','highlight');
+$('li:eq(2)').addClass('highlight');
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+ $('p:last').remove();
 
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+
+$a.click(function() {
+    $a.text('Added to cart');
+  });
+});
--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,35 @@
+$(document).ready(function() {
+
 /**
  * Toggles "done" class on <li> element
  */
+ $(document).on('click', 'li', function(e){
+  $(this).toggleClass('done');
+})
 
 /**
  * Delete element when delete link clicked
  */
+ $(document).on('click', 'a.delete', function(e){
+  $(this).parents('li').remove();
+})
+
 
 /**
  * Adds new list item to <ul>
  */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
+  const addListItem = function (e) {
 
-  // rest here...
-};
+    const text = $('input').val();
+
+    // rest here...
+    $('ul').append(`<li><span>${text}</span><a class="delete">Delete</a></li>`)
+  };
 
 // add listener for add
+ $(document).on('click', 'a.add-item', function(e){
+  addListItem();
+})
+
+
+});
